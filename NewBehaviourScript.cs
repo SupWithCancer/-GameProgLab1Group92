cc071b405fb202b36c78e33abf51ce2636b7eabc
supwithcancer <54910786+SupWithCancer@users.noreply.github.com>
2023-01-05 20:01:37 +0200

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float jumpForce = 15f;
+    private bool isGrounded = false;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+            Run();
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+    }
+    private void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+    }
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
+    }
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = collider.Length > 1;
+    }
+}
+
